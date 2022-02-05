f522d0e1f23fe0c897b9c2331f8e7323addcc958
Evan <ekprogamer13@gmail.com>
2022-02-05 15:38:17 -0500

    higher efficiency

--- arduino mouse.py
+++ arduino mouse.py
@@ -3,20 +3,17 @@ import pyautogui as pg
 
 # pg.FAILSAFE = False
 
-ser = serial.Serial("COM5", '9600')
+s = serial.Serial("COM5", '9600')
+
+""""Since 0,0 is in the top left corner of
+the screen in pyautogui, we have to
+subtract the max screen height from
+the value of y"""
 
 while True:
-    read = ser.readline().decode('ascii')
+    read = s.readline().decode('ascii')
     array = read.split()
     print(array)
-
-    x = int(array[2].replace(',', ''))
-    y = (1439 - int(array[5].replace(',', '')))
-    """"Since 0,0 is in the top left corner of
-    the screen in pyautogui, we have to
-    subtract the max screen height from
-    the value of y.
-    The minus 1 from the 1440 in the 
-    y var to prevent the mouse from 
-    triggering the failsafe."""
+    x = int(array[0])
+    y = 1440-int(array[1])
     pg.moveTo(int(x), int(y))
--- joystick-as-mouse-use-with-python/joystick-as-mouse-use-with-python.ino
+++ joystick-as-mouse-use-with-python/joystick-as-mouse-use-with-python.ino
@@ -3,15 +3,13 @@ void setup() {
   Serial.begin(9600);
 }
 
+// mappings for a 4k display
+
 void loop() {
-  // mappings for a 4k display
   int x = map(analogRead(A1), 0, 1023, 2560, 0); 
   int y = map(analogRead(A0), 0, 1023, 1440, 0);
-  
-  Serial.print("x = ");
   Serial.print(x);
-  Serial.print(" y = ");
+  Serial.print(' ');
   Serial.println(y);
-  
-  delay(200);
-  }
+  delay(100);
+}

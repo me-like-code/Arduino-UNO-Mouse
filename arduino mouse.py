97e47eeae45b2e4b968996f7d3ee5c7477c62866
Evan <ekprogamer13@gmail.com>
2022-02-06 15:42:25 -0500

    cleaned up

--- arduino mouse.py
+++ arduino mouse.py
@@ -1,16 +1,17 @@
 import serial
 from pyautogui import moveTo
 
-s = serial.Serial("COM5", '9600')
+s = serial.Serial('COM5', '9600')
 
-""""Since 0,0 is in the top left corner of
+'''Since 0,0 is in the top left corner of
 the screen in pyautogui, we have to
 subtract the max screen height from
-the value of y"""
+the value of y'''
 
 while True:
-    read = s.readline().decode('ascii')
+    read = s.readline().decode('ASCII')
     array = read.split()
     x = int(array[0])
     y = 1440-int(array[1])
     moveTo(int(x), int(y))
+
